17060914b7fbb2bf9da679c4b60cbaba4164365e
kolkatatestuser0143 <kolkatatestuser@gmail.com>
2026-08-13 14:33:50 +0530

    fix(api): load DNS bootstrap before Nest startup

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,3 +1,6 @@
+import './bootstrap-dns';
+import 'dotenv/config';
+
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
