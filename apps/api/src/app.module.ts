3217bb4e47bb12106d83867b59d5830bf0d80d60
kolkatatestuser0143 <kolkatatestuser@gmail.com>
2026-08-15 01:35:37 +0530

    feat(billing): register tenant entitlement service

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -30,6 +30,7 @@ import { IdentityService } from './modules/identity/identity.service';
 import { IdentityAdminController } from './modules/identity/identity-admin.controller';
 import { TenantLicenseController } from './modules/billing/tenant-license.controller';
 import { TenantLicenseService } from './modules/billing/tenant-license.service';
+import { EntitlementService } from './modules/billing/entitlement.service';
 import { SystemSubscriptionController } from './modules/billing/system-subscription.controller';
 import { SystemSubscriptionService } from './modules/billing/system-subscription.service';
 import { SystemAdminController } from './modules/system/system-admin.controller';
@@ -42,6 +43,6 @@ import { UsersService } from './modules/users/users.service';
 @Module({
  imports:[DatabaseModule,JwtModule.register({secret:process.env.JWT_ACCESS_SECRET,signOptions:{algorithm:'HS256'}}),ThrottlerModule.forRoot([{ttl:60000,limit:100}])],
  controllers:[AuthController,InviteController,AssetsController,AssetDocumentsController,WarrantyController,CustomFieldsController,TenancyController,RbacController,IdentityController,IdentityAdminController,TenantLicenseController,SystemSubscriptionController,SystemAdminController,AuditController,UsersController],
- providers:[MongooseDatabaseService,MailService,AuthService,ProvisioningService,SessionService,InviteService,AssetsService,AssetDocumentsService,WarrantyService,CustomFieldsService,TenancyService,RbacService,IdentityService,IdentitySecurityCacheService,TenantLicenseService,SystemSubscriptionService,SystemAdminService,AuditService,{provide:APP_INTERCEPTOR,useClass:AuditInterceptor},UsersService],
+ providers:[MongooseDatabaseService,MailService,AuthService,ProvisioningService,SessionService,InviteService,AssetsService,AssetDocumentsService,WarrantyService,CustomFieldsService,TenancyService,RbacService,IdentityService,IdentitySecurityCacheService,TenantLicenseService,EntitlementService,SystemSubscriptionService,SystemAdminService,AuditService,{provide:APP_INTERCEPTOR,useClass:AuditInterceptor},UsersService],
 })
 export class AppModule {}
