c2f61e4432b1c028c6454a519e5cf82a9d23966e
kolkatatestuser0143 <kolkatatestuser@gmail.com>
2026-08-15 14:04:28 +0530

    feat(ack): register acknowledgement template schema

--- apps/api/src/common/database/database.module.ts
+++ apps/api/src/common/database/database.module.ts
@@ -10,31 +10,17 @@ import { IdentityProviderConfigModelName, IdentityProviderConfigSchema, ScimToke
 import { IntegrationInstanceModelName, IntegrationInstanceSchema } from '../../models/integration.schemas';
 import { PlanModelName, PlanSchema, SubscriptionModelName, SubscriptionSchema, EntitlementModelName, EntitlementSchema } from '../../models/billing.schemas';
 import { AuditEventModelName, AuditEventSchema, PlatformAdminNoteModelName, PlatformAdminNoteSchema } from '../../models/audit.schemas';
-import { AssetReportTemplateModelName, AssetReportTemplateSchema } from '../../models/report.schemas';
+import { AssetReportTemplateModelName, AssetReportTemplateSchema, AssetAcknowledgementTemplateModelName, AssetAcknowledgementTemplateSchema } from '../../models/report.schemas';
 
 const MODEL_PROVIDERS = [
-  { name: TenantModelName, schema: TenantSchema }, { name: CompanyModelName, schema: CompanySchema },
-  { name: BusinessUnitModelName, schema: BusinessUnitSchema }, { name: PlantModelName, schema: PlantSchema },
-  { name: LocationModelName, schema: LocationSchema }, { name: DepartmentModelName, schema: DepartmentSchema },
-  { name: UserModelName, schema: UserSchema }, { name: SessionModelName, schema: SessionSchema },
-  { name: LoginHistoryModelName, schema: LoginHistorySchema }, { name: PermissionModelName, schema: PermissionSchema },
-  { name: RoleModelName, schema: RoleSchema }, { name: AssetTypeModelName, schema: AssetTypeSchema },
-  { name: AssetModelName, schema: AssetSchema }, { name: AssetAuditEventModelName, schema: AssetAuditEventSchema },
-  { name: AssetAssignmentModelName, schema: AssetAssignmentSchema }, { name: AssetTransferModelName, schema: AssetTransferSchema },
-  { name: AssetMaintenanceModelName, schema: AssetMaintenanceSchema },
-  { name: VendorModelName, schema: VendorSchema }, { name: WarrantyModelName, schema: WarrantySchema },
-  { name: CustomFieldDefModelName, schema: CustomFieldDefSchema }, { name: AssetCustomFieldValueModelName, schema: AssetCustomFieldValueSchema },
-  { name: AssetDocumentModelName, schema: AssetDocumentSchema }, { name: IdentityProviderConfigModelName, schema: IdentityProviderConfigSchema },
-  { name: ScimTokenModelName, schema: ScimTokenSchema }, { name: ScimSyncLogModelName, schema: ScimSyncLogSchema },
-  { name: IntegrationInstanceModelName, schema: IntegrationInstanceSchema }, { name: PlanModelName, schema: PlanSchema },
-  { name: SubscriptionModelName, schema: SubscriptionSchema }, { name: EntitlementModelName, schema: EntitlementSchema },
-  { name: AuditEventModelName, schema: AuditEventSchema }, { name: PlatformAdminNoteModelName, schema: PlatformAdminNoteSchema },
-  { name: AssetReportTemplateModelName, schema: AssetReportTemplateSchema },
+  { name: TenantModelName, schema: TenantSchema }, { name: CompanyModelName, schema: CompanySchema }, { name: BusinessUnitModelName, schema: BusinessUnitSchema }, { name: PlantModelName, schema: PlantSchema }, { name: LocationModelName, schema: LocationSchema }, { name: DepartmentModelName, schema: DepartmentSchema },
+  { name: UserModelName, schema: UserSchema }, { name: SessionModelName, schema: SessionSchema }, { name: LoginHistoryModelName, schema: LoginHistorySchema }, { name: PermissionModelName, schema: PermissionSchema }, { name: RoleModelName, schema: RoleSchema },
+  { name: AssetTypeModelName, schema: AssetTypeSchema }, { name: AssetModelName, schema: AssetSchema }, { name: AssetAuditEventModelName, schema: AssetAuditEventSchema }, { name: AssetAssignmentModelName, schema: AssetAssignmentSchema }, { name: AssetTransferModelName, schema: AssetTransferSchema }, { name: AssetMaintenanceModelName, schema: AssetMaintenanceSchema },
+  { name: VendorModelName, schema: VendorSchema }, { name: WarrantyModelName, schema: WarrantySchema }, { name: CustomFieldDefModelName, schema: CustomFieldDefSchema }, { name: AssetCustomFieldValueModelName, schema: AssetCustomFieldValueSchema }, { name: AssetDocumentModelName, schema: AssetDocumentSchema },
+  { name: IdentityProviderConfigModelName, schema: IdentityProviderConfigSchema }, { name: ScimTokenModelName, schema: ScimTokenSchema }, { name: ScimSyncLogModelName, schema: ScimSyncLogSchema }, { name: IntegrationInstanceModelName, schema: IntegrationInstanceSchema }, { name: PlanModelName, schema: PlanSchema }, { name: SubscriptionModelName, schema: SubscriptionSchema }, { name: EntitlementModelName, schema: EntitlementSchema },
+  { name: AuditEventModelName, schema: AuditEventSchema }, { name: PlatformAdminNoteModelName, schema: PlatformAdminNoteSchema }, { name: AssetReportTemplateModelName, schema: AssetReportTemplateSchema }, { name: AssetAcknowledgementTemplateModelName, schema: AssetAcknowledgementTemplateSchema },
 ];
 
 @Global()
-@Module({
-  imports: [MongooseModule.forRootAsync({ useFactory: () => ({ uri: process.env.MONGODB_URI }) }), MongooseModule.forFeature(MODEL_PROVIDERS)],
-  exports: [MongooseModule],
-})
+@Module({ imports: [MongooseModule.forRootAsync({ useFactory: () => ({ uri: process.env.MONGODB_URI }) }), MongooseModule.forFeature(MODEL_PROVIDERS)], exports: [MongooseModule] })
 export class DatabaseModule {}
