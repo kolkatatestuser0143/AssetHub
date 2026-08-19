cb7aeadcf4edef11e4ab7a840ba153f29e11676c
kolkatatestuser0143 <kolkatatestuser@gmail.com>
2026-08-19 16:46:52 +0530

    feat(web): add real Sites page

---
+++ apps/web/src/app/(protected)/plants/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import OrganizationLevelPage from '../../../components/organization/OrganizationLevelPage';
+
+export default function PlantsPage() {
+  return <OrganizationLevelPage level="sites" />;
+}
