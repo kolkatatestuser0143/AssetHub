39a94fd0cd23a4ded71a685d3ccacdbfb5b9a5cd
kolkatatestuser0143 <kolkatatestuser@gmail.com>
2026-08-19 16:51:28 +0530

    feat(web): activate Tenant Sites page

--- apps/web/src/app/(protected)/plants/page.tsx
+++ apps/web/src/app/(protected)/plants/page.tsx
@@ -1,5 +1,7 @@
-import { redirect } from 'next/navigation';
+'use client';
+
+import OrganizationLevelPage from '../../../components/organization/OrganizationLevelPage';
 
 export default function PlantsPage() {
-  redirect('/companies');
+  return <OrganizationLevelPage level="sites" />;
 }
