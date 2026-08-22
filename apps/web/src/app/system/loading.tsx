1c9d28e99197b0942193391f9632a368807e7118
kolkatatestuser0143 <kolkatatestuser@gmail.com>
2026-08-22 21:54:59 +0530

    Standardize System Admin route loading UI

---
+++ apps/web/src/app/system/loading.tsx
@@ -0,0 +1,5 @@
+import { LoadingState } from '../../components/loading-state';
+
+export default function Loading() {
+  return <LoadingState fullPage label="Loading System Console" description="Preparing platform control center…" />;
+}
