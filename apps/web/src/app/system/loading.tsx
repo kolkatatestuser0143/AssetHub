c95d8d29a5f23b4654cdd039635cce64b05cdfdb
kolkatatestuser0143 <kolkatatestuser@gmail.com>
2026-08-22 22:02:39 +0530

    Use identical LoadingSkeleton for System routes

--- apps/web/src/app/system/loading.tsx
+++ apps/web/src/app/system/loading.tsx
@@ -1,5 +1,5 @@
 import { LoadingSkeleton } from '../../components/loading-state';
 
 export default function Loading() {
-  return <LoadingSkeleton fullPage rows={6} label="Loading System Console" />;
+  return <LoadingSkeleton fullPage rows={6} label="Loading content" />;
 }
